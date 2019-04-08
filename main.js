7a236c29fd7b351b14957d3274bba2c5b34ee204
Rodrigo Alves <rodrigoalves009@gmail.com>
2019-04-08 18:32:25 -0300

    send error

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const dns = require('dns');
 
-let ipcRenderer = require('electron').ipcRenderer;
-ipcRenderer.send('submitForm', formData);
+// let ipcRenderer = require('electron').ipcRenderer;
+// ipcRenderer.send('submitForm', formData);
 
-// main.js
-ipcMain.on('submitForm', function(event, data) {
-   // Access form data here
-});
+// // main.js
+// ipcMain.on('submitForm', function(event, data) {
+//    // Access form data here
+// });
 
 function createWindow () {
 
@@ -22,9 +22,56 @@ function createWindow () {
       console.log("internet funcionando");
       win.loadFile("index.html");
     }
-  });
-  
 
+    const MainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(MainMenu);
+  });
+
+  win.on('closed', () =>{
+    app.quit();
+  });
 };
 
+function createAddWindow(){
+  addWindow = new BrowserWindow({ 
+    width: 450, 
+    height: 450, 
+    title: 'Adicionando carro'
+  });
+  addWindow.loadFile("index.html");
+}
+
+
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu:[
+      {
+        label: 'Add car',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Remove car'
+      }
+    ]
+  }
+];
+
 app.on('ready', createWindow);
+
+//Developer Mode
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle Dev Tools',
+        click(item, focusedWindow){
+          
+        }
+      }
+    ]
+  });
+}
