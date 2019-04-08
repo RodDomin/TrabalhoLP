c14fb94ca7f4a6771290530b1a0eb683b2c75552
Rodrigo Alves <rodrigoalves009@gmail.com>
2019-04-08 18:02:02 -0300

    alterações no back

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const dns = require('dns');
+
+let ipcRenderer = require('electron').ipcRenderer;
+ipcRenderer.send('submitForm', formData);
+
+// main.js
+ipcMain.on('submitForm', function(event, data) {
+   // Access form data here
+});
 
 function createWindow () {
-  // Create the browser window.
-  let win = new BrowserWindow({ width: 800, height: 600 })
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
-}
+  let win = new BrowserWindow({ width: 800, height: 600 });
 
-app.on('ready', createWindow)
+  dns.resolve('www.google.com',(error) =>{
+    if(error){
+      console.log("internet sem funcionar");
+      win.loadFile("Views/NoInternet.html");
+    }
+    else{
+      console.log("internet funcionando");
+      win.loadFile("index.html");
+    }
+  });
+  
+
+};
+
+app.on('ready', createWindow);
